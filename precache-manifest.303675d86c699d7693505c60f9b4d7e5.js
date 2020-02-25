ced8b6657917cbd190ab97e550d26a19b6d39ae1
Sushant-Stha <sshre733@mtroyal.ca>
2020-02-24 17:43:48 -0700

    Updates

---
+++ precache-manifest.303675d86c699d7693505c60f9b4d7e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359e61925bf3d8f51dc91b095df4a668",
+    "url": "/Web3Asg1/index.html"
+  },
+  {
+    "revision": "7d7233e4774257a76333",
+    "url": "/Web3Asg1/static/css/main.5d4f1e29.chunk.css"
+  },
+  {
+    "revision": "974363d575f94090bb2c",
+    "url": "/Web3Asg1/static/js/2.58f5f3e9.chunk.js"
+  },
+  {
+    "revision": "e13163c277047ad8a8c284f199a3fd56",
+    "url": "/Web3Asg1/static/js/2.58f5f3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7233e4774257a76333",
+    "url": "/Web3Asg1/static/js/main.4b109884.chunk.js"
+  },
+  {
+    "revision": "767f41a051d3a07ab984",
+    "url": "/Web3Asg1/static/js/runtime-main.707c1d37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web3Asg1/static/media/logo.5d5d9eef.svg"
+  }
+]);
